7ddc4e41f75f685933d262909c2e2b4a5ae740bc
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2014-11-26 15:48:07 +0100

    Added test for UrlParamsProcessor plus composer update

--- tests/UrlParamsProcessorTest.php
+++ tests/UrlParamsProcessorTest.php
@@ -1,4 +1,5 @@
 <?php
+use Gzero\Api\UrlParamsProcessor;
 
 /**
  * This file is part of the GZERO CMS package.
@@ -14,24 +15,84 @@
 class UrlParamsProcessorTest extends \PHPUnit_Framework_TestCase {
 
     /**
+     * @var UrlParamsProcessor
+     */
+    protected $processor;
+
+    protected function setUp()
+    {
+        $this->processor = $this->initClass();
+    }
+
+    /**
      * @test
      */
-    public function is_working()
+    public function is_instantiable()
     {
-        $processor = new \Gzero\Api\UrlParamsProcessor([
-            'sort'         => '-test1,test2,-test3',
-            'page'         => 3,
-            'filter_param' => 'test'
-        ]);
+        $this->assertInstanceOf('\Gzero\Api\UrlParamsProcessor', $this->processor);
+    }
+
+    /**
+     * @test
+     */
+    public function can_filter_params()
+    {
+        $this->assertEquals($this->processor->getFilterParams(), ['lang' => 'en', 'test2' => 'test2']);
+    }
+
+    /**
+     * @test
+     */
+    public function is_returning_page_params()
+    {
+        $this->assertEquals($this->processor->getPage(), 3);
+        $this->assertEquals($this->processor->getPerPage(), 21);
+    }
+
+    /**
+     * @test
+     */
+    public function can_process_sort_params()
+    {
+        $this->assertEquals($this->processor->getOrderByParams(), ['test1' => 'DESC', 'test2' => 'ASC', 'test3' => 'ASC']);
+    }
+
+    /**
+     * @test
+     */
+    public function is_returning_processed_fields_in_correct_format()
+    {
         $this->assertEquals(
-            $processor->getOrderByParams(),
+            $this->processor->getProcessedFields(),
             [
-                'test1' => 'DESC',
-                'test2' => 'ASC',
-                'test3' => 'DESC'
+                'page'    => 3,
+                'perPage' => 21,
+                'filer'   => [
+                    'lang'  => 'en',
+                    'test2' => 'test2'
+                ],
+                'orderBy' => [
+                    'test1' => 'DESC',
+                    'test2' => 'ASC',
+                    'test3' => 'ASC'
+                ]
             ]
         );
-        $this->assertEquals($processor->getPage(), 3);
-        $this->assertEquals($processor->getFilterParams(), ['filter_param' => 'test']);
+    }
+
+    /**
+     * @return UrlParamsProcessor
+     */
+    protected function initClass()
+    {
+        return new UrlParamsProcessor(
+            [
+                'sort'    => '-test1,test2,test3',
+                'page'    => 3,
+                'perPage' => 21,
+                'lang'    => 'en',
+                'test2'   => 'test2'
+            ]
+        );
     }
 }
