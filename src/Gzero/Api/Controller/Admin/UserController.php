151859d2d73d181f8c1f61e0e9927cdf3b40f12a
mateusz <mateusz@localhost.localdomain>
2015-05-06 19:01:22 +0200

    KMS-40 UserValidator and index user api action

---
+++ src/Gzero/Api/Controller/Admin/UserController.php
@@ -0,0 +1,16 @@
+<?php namespace Gzero\Api\Controller\Admin;
+
+/**
+ * This file is part of the GZERO CMS package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Class UserController
+ * @package    Gzero\Api\Controller\Admin
+ * @author     Adrian Skierniewski <adrian.skierniewski@gmail.com>
+ * @copyright  Copyright (c) 2015, Adrian Skierniewski
+ */
+class UserController {
+
+}
---
+++ src/Gzero/Api/Validator/UserValidator.php
@@ -0,0 +1,16 @@
+<?php namespace Gzero\Api\Validator;
+
+/**
+ * This file is part of the GZERO CMS package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Class UserValidator
+ * @package    Gzero\Api\Validator
+ * @author     Adrian Skierniewski <adrian.skierniewski@gmail.com>
+ * @copyright  Copyright (c) 2015, Adrian Skierniewski
+ */
+class UserValidator {
+
+}
