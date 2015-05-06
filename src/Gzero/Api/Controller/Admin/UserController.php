06471a19594f121b7f82acfcb116854f0623ed33
mateusz <mateusz@localhost.localdomain>
2015-05-06 19:01:28 +0200

    KMS-40 UserValidator and index user api action

--- src/Gzero/Api/Controller/Admin/UserController.php
+++ src/Gzero/Api/Controller/Admin/UserController.php
@@ -1,5 +1,11 @@
 <?php namespace Gzero\Api\Controller\Admin;
 
+use Gzero\Api\Controller\ApiController;
+use Gzero\Repository\UserRepository;
+use Gzero\Api\Transformer\UserTransformer;
+use Gzero\Api\UrlParamsProcessor;
+use Gzero\Api\Validator\UserValidator;
+
 /**
  * This file is part of the GZERO CMS package.
  *
@@ -7,10 +13,66 @@
  * file that was distributed with this source code.
  *
  * Class UserController
+ *
  * @package    Gzero\Api\Controller\Admin
- * @author     Adrian Skierniewski <adrian.skierniewski@gmail.com>
- * @copyright  Copyright (c) 2015, Adrian Skierniewski
+ * @author     Mateusz Urbanowicz <urbanowiczmateusz89@gmail.com>
+ * @copyright  Copyright (c) 2015, Mateusz Urbanowicz
  */
-class UserController {
+class UserController extends ApiController {
+
+    /**
+     * @var UserRepository
+     */
+    protected $userRepo;
+
+    /**
+     * ContentController constructor.
+     *
+     * @param UrlParamsProcessor $processor Url processor
+     * @param ContentRepository  $content   Content repository
+     * @param ContentValidator   $validator Content validator
+     */
+    public function __construct(UrlParamsProcessor $processor, UserRepository $content, UserValidator $validator)
+    {
+        parent::__construct($processor);
+        $this->validator = $validator->setData(\Input::all());
+        $this->userRepo  = $content;
+    }
+
+    /**
+     * Display list of users
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $input   = $this->validator->validate('list');
+        $params  = $this->processor->process($input)->getProcessedFields();
+        $results = $this->userRepo->retrieveUsers(
+            $params['filter'],
+            $params['orderBy'],
+            $params['page'],
+            $params['perPage']
+        );
+        return $this->respondWithSuccess($results, new UserTransformer);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param $id User ID
+     *
+     * @return Response
+     */
+    public function show($id)
+    {
+        $user = $this->userRepo->retrieveById($id);
+        if (empty($user)) {
+            return $this->respondNotFound();
+        } else {
+            return $this->respondWithSuccess($user, new UserTransformer);
+        }
+    }
+
 
 }
--- src/Gzero/Api/Validator/UserValidator.php
+++ src/Gzero/Api/Validator/UserValidator.php
@@ -1,16 +1,36 @@
 <?php namespace Gzero\Api\Validator;
 
+use Gzero\Validator\AbstractValidator;
+
 /**
  * This file is part of the GZERO CMS package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * Class UserValidator
+ * Class ContentParamsValidator
+ *
  * @package    Gzero\Api\Validator
- * @author     Adrian Skierniewski <adrian.skierniewski@gmail.com>
- * @copyright  Copyright (c) 2015, Adrian Skierniewski
+ * @author     Adrian Skierniewski <urbanowiczmateusz89@gmail.com.com>
+ * @copyright  Copyright (c) 2015, Mateusz Urbanowicz
  */
-class UserValidator {
+class UserValidator extends AbstractValidator {
 
+    /**
+     * @var array
+     */
+    protected $rules = [
+        'list' => [
+            'page'     => 'numeric',
+            'perPage'  => 'numeric',
+            'sort'     => '',
+        ],
+    ];
+
+    /**
+     * @var array
+     */
+    protected $filters = [
+        'title' => 'trim'
+    ];
 }
