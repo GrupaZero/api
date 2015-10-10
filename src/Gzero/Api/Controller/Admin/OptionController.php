075b7f88445015331e542f37bf7a6f83e546822a
Przemyslaw Skierniewski <skierniewski.przemyslaw@gmail.com>
2015-10-10 21:50:57 +0200

    Options api typo fix KMS-172

--- src/Gzero/Api/Controller/Admin/OptionController.php
+++ src/Gzero/Api/Controller/Admin/OptionController.php
@@ -12,7 +12,7 @@ use Gzero\Repository\OptionRepository;
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * Class LangController
+ * Class OptionController
  *
  * @package    Gzero\Admin\Controllers\Resource
  * @author     Adrian Skierniewski <adrian.skierniewski@gmail.com>
